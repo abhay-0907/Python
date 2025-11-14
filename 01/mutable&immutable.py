ee7025649240bf4df896e677ddc7e8275e14430e
abhay-0907 <info.abhaysharma09@gmail.com>
2025-11-14 18:52:23 +0530

    content added

--- 01/mutable&immutable.py
+++ 01/mutable&immutable.py
@@ -1,3 +1,10 @@
+'''
+mutable: list, dict, set
+immutable: int, float, string, bool, tuple
+Mutabel -> Mutable means their internal state (content) can be changed without changing their memory address.
+Immutable -> Immutable means their internal state (content) cannot be changed without changing their memory address. Any "change" results in a new object being created, so memory reference changes.
+'''
+
 num_1 = 10
 num_2 = num_1
 num_1 = 15
